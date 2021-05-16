fd7c463597f8d69ccf6307f168adf4ae85760095
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-05-16 17:08:47 +0300

    local storage implemented for auth datas

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,20 +1,38 @@
 import { createStore } from "redux";
 import authReducer from "./authReducer";
 
-const loggedInState = {
-  isLoggedIn: true,
-  username: "asd",
-  name: "adqweqwe",
-  image: undefined,
-  password: "passs",
-};
+import * as ACTIONS from "./Constants";
 
 const configureStore = () => {
-  return createStore(
+  const authData = localStorage.getItem(ACTIONS.AUTH_DATA);
+
+  let stateInLocalStorage = {
+    isLoggedIn: false,
+    username: undefined,
+    name: undefined,
+    image: undefined,
+    password: undefined,
+  };
+
+  if (authData) {
+    try {
+      stateInLocalStorage = JSON.parse(authData);
+    } catch (error) {
+      console.log("problem");
+    }
+  }
+
+  const store = createStore(
     authReducer,
-    //loggedInState,
+    stateInLocalStorage,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  store.subscribe(() => {
+    localStorage.setItem(ACTIONS.AUTH_DATA, JSON.stringify(store.getState()));
+  });
+
+  return store;
 };
 
 export default configureStore;
