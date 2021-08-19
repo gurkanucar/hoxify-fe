490340055eac71c2f85f20f99845d12a4f70e875
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-08-19 15:34:23 +0300

    User Profile Card updated

--- src/Components/ProfileCard.js
+++ src/Components/ProfileCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import defaultPicture from "../assets/profile.png";
 
 const ProfileCard = (props) => {
   const { username: loggedInUsername } = useSelector((store) => ({
@@ -9,15 +10,34 @@ const ProfileCard = (props) => {
 
   const routeParams = useParams();
 
+  const { user } = props;
+  const { username, name, image } = user;
+
+  let imageSource = defaultPicture;
+  if (image) {
+    imageSource = image;
+  }
+
   const pathUsername = routeParams.username;
   let message = "We cannot edit!";
   if (pathUsername === loggedInUsername) {
     message = "You can edit your profile.";
   }
   return (
-    <div>
-      <h3>Profile Card</h3>
-      <h5>{message}</h5>
+    <div className="card text-center shadow">
+      <div className="card-header">
+        <img
+          className="rounded-circle shadow"
+          width="200"
+          alt="img"
+          src={imageSource}
+        />
+      </div>
+      <div className="card-body">
+        <h3>
+          {username}@{name}
+        </h3>
+      </div>
     </div>
   );
 };
--- src/Pages/UserPage.js
+++ src/Pages/UserPage.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProfileCard from "../components/ProfileCard";
+import { getUser } from "../api/apiCalls";
+import { useParams } from "react-router";
+import { useTranslation } from "react-i18next";
 
-const UserPage = (props) => {
+const UserPage = () => {
+  const { t } = useTranslation();
+  const [user, setUser] = useState({});
+  const [notFound, setNotFound] = useState(false);
+
+  const { username } = useParams();
+
+  useEffect(() => {
+    setNotFound(false);
+  }, [user]);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        const response = await getUser(username);
+        setUser(response.data);
+      } catch (error) {
+        setNotFound(true);
+      }
+    };
+
+    loadUser();
+  }, [username]);
+
+  if (notFound) {
+    return (
+      <div className="container">
+        <div className="alert alert-danger text-center">
+          <div>
+            <span class="material-icons" style={{ fontSize: "48px" }}>
+              error
+            </span>
+          </div>
+          {t("User Not Found")}
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="container">
       <h1>User Profile</h1>
-      <ProfileCard />
+      <ProfileCard user = {user} />
     </div>
   );
 };
